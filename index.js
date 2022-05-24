0cb7665ae51becf72864f09779d08a051badb18e
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-05-24 15:00:39 +0200

    Index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+
+import connectDB from './config/db.js'
+import postsRouter from './routes/post.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 4050
+
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/posts', postsRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
