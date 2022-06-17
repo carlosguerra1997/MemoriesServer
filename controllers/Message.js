4b40bc9bf7a34dceb609dda3c588f0ebdccd0d15
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-17 14:17:10 +0200

    Implementing routes & controllers

--- controllers/Message.js
+++ controllers/Message.js
@@ -6,7 +6,7 @@ export const createPost = async (req, res) => {
     await post.save()
     res.status(200).json(post)
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(404).send(error)
   }
 }
 
@@ -15,6 +15,39 @@ export const getPosts = async (req, res) => {
     const posts = await Post.find()
     res.status(200).json(posts)
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(404).send(error)
   }
 }
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params
+  const { post } = req.body
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(id, post, { new: true })
+    res.status(200).json(updatedPost)
+  } catch (error) {
+    res.status(404).send(error)
+  }
+}
+
+export const likePost = async (req, res) => {
+  const { id } = req.params
+  try {
+    const post = await Post.findById(id)
+    const updatedPostLikes = await Post.findByIdAndUpdate(id, { likes: post.likes + 1 }, { new: true })
+    return res.status(200).json(updatedPostLikes)
+  } catch (error) {
+    res.status(404).send(error)
+  }
+}
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params
+  try {
+    await Post.findByIdAndDelete(id)
+    res.status(200).json({ ok: true, message: 'Post borrado con éxito' })
+  } catch (error) {
+    res.status(404).send(error)
+  }
+}
+
--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
-import { createPost, getPosts } from '../controllers/Message.js'
+import { createPost, deletePost, likePost, getPosts, updatePost } from '../controllers/Message.js'
 
 const router = express.Router()
 
 router.post('/', createPost)
 router.get('/', getPosts)
+router.put('/:id', updatePost)
+router.put('/:id/like', likePost)
+router.delete('/:id', deletePost)
 
 export default router
