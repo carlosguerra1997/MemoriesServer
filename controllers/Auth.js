0c786867f89b15bb13a39b2e55e7ae71c5f2399f
carlosguerra1997 <guerragcarlos97@gmail.com>
2022-06-21 20:43:50 +0200

    Implementing auth routes

---
+++ controllers/Auth.js
@@ -0,0 +1,16 @@
+export const signIn = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(400).send({ ok: false, msg: error })
+  }
+}
+
+export const signUp = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(400).send({ ok: false, msg: error })
+  }
+}
+
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from 'express'
 
 import connectDB from './config/db.js'
 import postsRouter from './routes/post.js'
+import authRouter from './routes/auth.js'
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(cors())
 app.use(express.json({ limit: '10MB' }))
 
 app.use('/api/posts', postsRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
