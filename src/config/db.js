5f83f029b28a8f89f645e0e65bab6d987410a534
Carlos Guerra <guerragcarlos97@gmail.com>
2022-07-08 12:51:53 +0200

    Small fixes

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
+    console.log('DB connected successfully')
   } catch (error) {
     console.log(error)
     process.exit(1)
